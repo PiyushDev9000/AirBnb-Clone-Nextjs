8b4770dfbbebe20079cab4dae3db0d53cc7a1efe
piyushdevgoku <piyushnarwal2002@gmail.com>
2023-11-15 17:55:44 +0530

    navbar completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,33 @@
-import type { Metadata } from 'next'
-import { Nunito } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 
 const font = Nunito({
-  subsets: ['latin']
-})
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb Clone',
-}
+  title: "Airbnb",
+  description: "Airbnb Clone",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <ClientOnly>
+          <Modal />
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
